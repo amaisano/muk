82dfbe907445b8658aef8a5fad4434869182f0a1
Adamo Maisano <atwixtor@gmail.com>
2022-04-16 02:33:06 -0400

    Revert "attempt to fix rate limiting security concern (2 of 2)"
    This reverts commit 85bea078cacf7a4f96b9633e10031d1c58fa4d6c.

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-// Initialize module at 100 requests per 10 seconds:
-var rateLimit = require('ws-rate-limit')(100, '10s')
-
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -16,9 +13,6 @@ const server = express()
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
-  // Apply rate limiting to client
-  rateLimit(ws);
-
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
