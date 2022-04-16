d930631b1f6ab06e739ee2f03d47eee1e52eb75c
Adamo Maisano <atwixtor@gmail.com>
2022-04-16 02:07:00 -0400

    env switching + optimize script location
    - also ignore vscode settings

---
+++ js/websocket.js
@@ -0,0 +1,14 @@
+if (window.location.hostname.indexOf("lndo") > 0) {
+  var HOST = 'ws://muk_ws.lndo.site';
+}
+else {
+  var HOST = location.origin.replace(/^http/, 'ws');
+}
+
+let ws = new WebSocket(HOST);
+let el;
+
+ws.onmessage = (event) => {
+  el = document.getElementById('server-time');
+  el.innerHTML = 'Server time: ' + event.data;
+};
