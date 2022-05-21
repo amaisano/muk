2882ce895cfae164e8e8ab130991700b1f45057f
Adamo Maisano <atwixtor@gmail.com>
2022-05-21 02:47:15 -0400

    persistent memory (after browser close)
    - may or may not work on streaming software overlays

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 
-  var chestCount = 0;
+  var chestCount = localStorage.getItem('count') ?? 0;
   var container = $("#container");
   var chest = "<div class='chest-wrapper'><div class='chest'></div><div class='sparkle-cw'></div><div class='sparkle-ccw'></div></div>";
+
+  // Recall count
+  $("#count").val(chestCount);
 
   // Add a new chest
   $("#add").click(function(){
     container.append(chest);
     chestCount++;
+    localStorage.setItem('count', chestCount);
   });
 
   // Remove last chest
@@ -15,6 +19,7 @@ $(document).ready(function(){
     if (chestCount > 0) {
       container.children().last().remove();
       chestCount--;
+      localStorage.setItem('count', chestCount);
     }
   });
 
@@ -22,6 +27,7 @@ $(document).ready(function(){
   $("#clear").click(function(){
     container.empty();
     chestCount = 0;
+    localStorage.setItem('count', chestCount);
   });
 
   // Update count when adding, removing, clearing
