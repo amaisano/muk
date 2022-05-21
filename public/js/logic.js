2f1bba967faef04ba27a674c6fb910f636d96149
Adamo Maisano <atwixtor@gmail.com>
2022-05-21 15:16:10 -0400

    remove document.ready wrapper
    - makes all functions and variables global
    - accessible from other js files and main index.html

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,58 +1,54 @@
-$(document).ready(function(){
+var chestCount = refill = localStorage.getItem('count') ?? 0;
+var container = $("#container");
+var chest = "<div class='chest-wrapper'><div class='chest'></div><div class='sparkle-cw'></div><div class='sparkle-ccw'></div></div>";
 
-  var chestCount = refill = localStorage.getItem('count') ?? 0;
-  var container = $("#container");
-  var chest = "<div class='chest-wrapper'><div class='chest'></div><div class='sparkle-cw'></div><div class='sparkle-ccw'></div></div>";
+// Recall count
+$("#count").val(chestCount);
 
-  // Recall count
-  $("#count").val(chestCount);
+// Fill recalled chests
+while (refill > 0) {
+  container.append(chest);
+  refill--;
+}
 
-  // Fill recalled chests
-  while (refill > 0) {
-    container.append(chest);
-    refill--;
-  }
+// Add a new chest
+$("#add").click(function(){
+  container.append(chest);
+  chestCount++;
+  localStorage.setItem('count', chestCount);
+});
 
-  // Add a new chest
-  $("#add").click(function(){
-    container.append(chest);
-    chestCount++;
-    localStorage.setItem('count', chestCount);
-  });
-
-  // Remove last chest
-  $("#remove").click(function(){
-    if (chestCount > 0) {
-      container.children().last().remove();
-      chestCount--;
-      localStorage.setItem('count', chestCount);
-    }
-  });
-
-  // Clear all chests
-  $("#clear").click(function(){
-    container.empty();
-    chestCount = 0;
-    localStorage.setItem('count', chestCount);
-  });
-
-  // Update count when adding, removing, clearing
-  $("#controls button").click(function() {
-    $("#count").val(chestCount);
-  });
-
-  // Remove the chest you click on
-  $("#container").on("click", "div.chest-wrapper", function(e){
-    $(this).hide("fast", done(e));
-  });
-
-  // "on" delegated element events need a static callback function
-  function done(event) {
-    // Event is actually propagated on the .chest element
-    $(event.target).parents('div.chest-wrapper').remove();
+// Remove last chest
+$("#remove").click(function(){
+  if (chestCount > 0) {
+    container.children().last().remove();
     chestCount--;
     localStorage.setItem('count', chestCount);
-    $("#count").val(chestCount);
   }
-
 });
+
+// Clear all chests
+$("#clear").click(function(){
+  container.empty();
+  chestCount = 0;
+  localStorage.setItem('count', chestCount);
+});
+
+// Update count when adding, removing, clearing
+$("#controls button").click(function() {
+  $("#count").val(chestCount);
+});
+
+// Remove the chest you click on
+$("#container").on("click", "div.chest-wrapper", function(e){
+  $(this).hide("fast", done(e));
+});
+
+// "on" delegated element events need a static callback function
+function done(event) {
+  // Event is actually propagated on the .chest element
+  $(event.target).parents('div.chest-wrapper').remove();
+  chestCount--;
+  localStorage.setItem('count', chestCount);
+  $("#count").val(chestCount);
+}
